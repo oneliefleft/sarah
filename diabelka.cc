ca9ab10ee39b96af40dd8f0ee7617b496461b18a
oneliefleft <oneliefleft@gmail.com>
2017-02-28 11:16:17 +0100

    Add command line parser.

--- diabelka.cc
+++ diabelka.cc
@@ -17,9 +17,23 @@ int main (int argc, char *argv[])
   // Initialise MPI
   dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
   
+  std::vector<std::string> args (argv+1, argv+argc);
+
+  std::cout << std::endl << std::endl
+	    << "----------------------------------------------------"
+	    << std::endl
+	    << "Caught arguments: ";
+  for (unsigned int i=0; i<args.size (); ++i)
+    std::cout << args[i] << " ";
+  std::cout  << std::endl
+	     << "----------------------------------------------------"
+	     << std::endl << std::endl;
+
+  AssertThrow (args.size ()>0, dealii::ExcMessage ("The number of input arguments must be greater than zero."));
+
   try
     {
-      sarah::CatProblem<2> diabelka ("diabelka.prm");
+      sarah::CatProblem<2> diabelka (args[0]);
       diabelka.run ();
     }
 
