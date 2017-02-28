ef8655ffd724be5845743398b31730c28a646f8a
oneliefleft <oneliefleft@gmail.com>
2017-02-28 11:22:21 +0100

    Improve input parser.

--- diabelka.cc
+++ diabelka.cc
@@ -17,22 +17,22 @@ int main (int argc, char *argv[])
   // Initialise MPI
   dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
   
-  std::vector<std::string> args (argv+1, argv+argc);
-
-  std::cout << std::endl << std::endl
-	    << "----------------------------------------------------"
-	    << std::endl
-	    << "Caught arguments: ";
-  for (unsigned int i=0; i<args.size (); ++i)
-    std::cout << args[i] << " ";
-  std::cout  << std::endl
-	     << "----------------------------------------------------"
-	     << std::endl << std::endl;
-
-  AssertThrow (args.size ()>0, dealii::ExcMessage ("The number of input arguments must be greater than zero."));
-
   try
     {
+      std::vector<std::string> args (argv+1, argv+argc);
+      
+      AssertThrow (args.size ()>0, dealii::ExcMessage ("The number of input arguments must be greater than zero."));
+      
+      std::cout << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl
+		<< "Caught arguments: ";
+      for (unsigned int i=0; i<args.size (); ++i)
+	std::cout << std::endl << "   " << args[i];
+      std::cout  << std::endl
+		 << "----------------------------------------------------"
+	     << std::endl << std::endl;
+      
       sarah::CatProblem<2> diabelka (args[0]);
       diabelka.run ();
     }
